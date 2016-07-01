bb94185571ddcf29c2fcef63dc8890bafdee86be
Rajat <sinharajatit@hotmail.com>
2016-07-01 07:48:14 +0000

    using jade template/any other template

---
+++ jade.js
@@ -0,0 +1,22 @@
+var express = require("express");
+ var app = express();
+//var path = require("path");
+
+// views: the directory where the template files are located
+app.set('views', process.argv[3]);
+
+//view engine: the template engine to use. For example, to use the jade template engine
+app.set('view engine', 'jade');
+
+app.get('/home', function(req, res){
+    
+    
+    res.render('index', {date: new Date().toDateString()});
+
+});
+
+app.listen(process.argv[2]);
+
+
+
+//console.log(process.argv[3]);
